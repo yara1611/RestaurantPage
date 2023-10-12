2010b61fcf04c04e50907b45bd66e369cf8b8980
yara <76455290+yara1611@users.noreply.github.com>
2023-10-12 20:20:23 +0300

    h

--- src/restaurant.js
+++ src/restaurant.js
@@ -11,7 +11,7 @@ const createRestHomePage = () =>{
 
     //Image
     const img = document.createElement('img')
-    img.src='https://ca-times.brightspotcdn.com/dims4/default/5eb6513/2147483647/strip/true/crop/2880x1920+0+0/resize/1440x960!/format/webp/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fec%2F5b%2Fdced3ab54ebab4a1477f4168f737%2F938247-tn-wknd-me-outdoor-patio-dining-chachas-20220410-12.jpg'
+    img.src='https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60'
     img.width='300'
     pageContent.appendChild(img)
 
